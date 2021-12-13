0a054b00254b0016357c957eb7d296b22851e518
Nikita Keler <nikita@macairsk.local>
2021-12-13 22:18:24 +0300

    init

---
+++ struct-1/struct-1/struct-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  struct-1
+//
+//  Created by Nikita Keler on 13.12.21.
+//  Copyright © 2021 Nikita Keler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
