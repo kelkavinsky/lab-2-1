6adfe3c658ffd97aea3481817605234ca475ccaf
Nikita Keler <nikita@MacAirSK.local>
2021-12-13 22:52:23 +0300

    task2 - ура

---
+++ Task2/Task2/main.cpp
@@ -0,0 +1,59 @@
+//
+//  main.cpp
+//  Task2
+//
+//  Created by Nikita Keler on 03.12.21.
+//  Copyright © 2021 Nikita Keler. All rights reserved.
+//
+
+#include "task2.hpp"
+
+Train::Train() {
+    this->name = "";
+    this->route = "";
+    this->count = 0;
+}
+
+Train::Train(string n, string r, int c) {
+    this->name = n;
+    this->route = r;
+    this->count = c;
+}
+
+string Train::getName() {
+    return this->name;
+}
+
+string Train::setName(string n) {
+    string s = this->name;
+    this->name = n;
+    return s;
+}
+
+string Train::getRoute() {
+    return this->route;
+}
+
+string Train::setRoute(string r) {
+    string s = this->route;
+    this->route = r;
+    return s;
+}
+
+int Train::getCount() {
+    return this->count;
+}
+
+int Train::setCount(int c) {
+    int s = this->count;
+    this->count = c;
+    return s;
+}
+
+int main(void) {
+    Train *t = new Train("qaz","wsx",5);
+    cout << t->getName() << endl;
+    cout << t->getRoute() << endl;
+    cout << t->getCount() << endl;
+}
+
---
+++ Task2/Task2/task2.hpp
@@ -0,0 +1,37 @@
+//
+//  task2.hpp
+//  Task1
+//  10 вариант
+//  Количество строк: 4. Столбцы: Название турфирмы, Маршрут, Количество оставшихся путевок. Вычислить общее количество оставшихся путевок.
+//  Created by Nikita Keler on 02.12.21.
+//  Copyright © 2021 Nikita Keler. All rights reserved.
+//
+
+#ifndef task2_hpp
+#define task2_hpp
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Train {
+private:
+    string name;
+    string route;
+    int count;
+public:
+    Train();
+    ~Train();
+    Train(string n, string r, int c);
+    string getName();
+    string setName(string n);
+    string getRoute();
+    string setRoute(string r);
+    int getCount();
+    int setCount(int c);
+};
+
+#endif /* task2_hpp */
+
